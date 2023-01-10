5c0c59a46d928526b80f399c061a4a602237083e
Scott Yuan <scoworthyuan@gmail.com>
2023-01-11 06:30:30 +1100

    removed duplications in accounts and improved execution performance.

--- email-filter.py
+++ email-filter.py
@@ -17,9 +17,6 @@ from openpyxl.xml import constants as openpyxl_xml_constants
 from pandas import ExcelFile
 from pandas.io.excel._openpyxl import OpenpyxlReader
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.settings.basic']
-blacklist = '/Users/scott/Library/CloudStorage/OneDrive-Personal/email-blacklist.xlsx'
 
 class OpenpyxlReaderWOFormatting(OpenpyxlReader):
     """OpenpyxlReader without reading formatting
@@ -47,258 +44,83 @@ class OpenpyxlReaderWOFormatting(OpenpyxlReader):
 
         return new_read
 
+
 ExcelFile._engines['openpyxl_wo_formatting'] = OpenpyxlReaderWOFormatting
 
 
-def scottpersonal():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    accountname = 'Scott Personal Account'
-    path = '/Users/scott/Learning/Coding/Python3/email-filter/credentials/personal-account'
-    if os.path.exists(f'{path}/token.json'):
-        creds = Credentials.from_authorized_user_file(f'{path}/token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                f'{path}/credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open(f'{path}/token.json', 'w') as token:
-            token.write(creds.to_json())
-    df = pd.read_excel(blacklist, engine='openpyxl_wo_formatting')
+def emailFilter():
+    # Gmail API Scope
+    SCOPES = ['https://www.googleapis.com/auth/gmail.settings.basic']
 
-    try:
-        # Call the Gmail API
-        service = build('gmail', 'v1', credentials=creds)
-        existingfilters = service.users().settings().filters().list(userId='me').execute()
-        # list filters
-        existingfilter = []
-        for item in existingfilters['filter']:
-            if 'from' in item['criteria'].keys():
-                existingfilter.append(item['criteria']['from'])
-        #check existing filters
+    # Email Acount Setup
+    paths = []
+    account_name = ['personal-account', 'scott-work-account', 'cafe-account', 'roaster-account']
+    for account in account_name:
+        paths.append(f'/Users/scott/Learning/Coding/Python3/auto-tasks/credentials/{account}')
+    # modify the path to the correct credential folder
 
-        emails = df['Email Black List'].tolist()
-        emailadded = False
-        for email in emails:
-            if email not in existingfilter:
-                filter_content = {
-                    'criteria': {
-                        'from': email
-                    },
-                    'action': {
-                        'addLabelIds': ['TRASH'],
-                        'removeLabelIds': ['INBOX']
+    # read blacklist
+    ExcelFile._engines['openpyxl_wo_formatting'] = OpenpyxlReaderWOFormatting
+    BLACK_LIST = '/Users/scott/Library/CloudStorage/OneDrive-Personal/email-blacklist.xlsx'
+    df = pd.read_excel(BLACK_LIST, engine='openpyxl_wo_formatting')
+    emails = df['Email Black List'].tolist()
+
+    for path in paths:
+        creds = None
+        if os.path.exists(f'{path}/token.json'):
+            creds = Credentials.from_authorized_user_file(f'{path}/token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    f'{path}/credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open(f'{path}/token.json', 'w') as token:
+                token.write(creds.to_json())
+
+        try:
+            # Call the Gmail API
+            service = build('gmail', 'v1', credentials=creds)
+            existingfilters = service.users().settings().filters().list(userId='me').execute()
+            # list filters
+            existingfilter = []
+            for item in existingfilters['filter']:
+                if 'from' in item['criteria'].keys():
+                    existingfilter.append(item['criteria']['from'])
+            # check existing filters
+
+            for email in emails:
+                if email not in existingfilter:
+                    filter_content = {
+                        'criteria': {
+                            'from': email
+                        },
+                        'action': {
+                            'addLabelIds': ['TRASH'],
+                            'removeLabelIds': ['INBOX']
+                        }
                     }
-                }
-                addFilter = service.users().settings().filters().create(userId='me', body=filter_content).execute()
-                emailadded = True
-                print(f'{email} added to {accountname} blacklist.')
+                    service.users().settings().filters().create(userId='me', body=filter_content).execute()
+                    accountname = path.split('/')[-1]
+                    print(f'{email} added to {accountname} blacklist.')
 
-        if not emailadded:
-            print(f'No new email added to {accountname}.')
+        except HttpError as error:
+            print(f'An error occurred: {error}')
+    current_time = time.ctime(time.time())
+    print(f"Email Blacklist Checked: {current_time}")
+    print("----------------------")
 
-    except HttpError as error:
-        print(f'An error occurred: {error}')
-def roaster():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    creds = None
-    accountname = 'Roaster Account'
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    path = '/Users/scott/Learning/Coding/Python3/email-filter/credentials/roaster-account'
-    if os.path.exists(f'{path}/token.json'):
-        creds = Credentials.from_authorized_user_file(f'{path}/token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                f'{path}/credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open(f'{path}/token.json', 'w') as token:
-            token.write(creds.to_json())
-    df = pd.read_excel(blacklist, engine='openpyxl_wo_formatting')
-
-    try:
-        # Call the Gmail API
-        service = build('gmail', 'v1', credentials=creds)
-        existingfilters = service.users().settings().filters().list(userId='me').execute()
-        # list filters
-        existingfilter = []
-        for item in existingfilters['filter']:
-            if 'from' in item['criteria'].keys():
-                existingfilter.append(item['criteria']['from'])
-        #check existing filters
-
-        emails = df['Email Black List'].tolist()
-        emailadded = False
-        for email in emails:
-            if email not in existingfilter:
-                filter_content = {
-                    'criteria': {
-                        'from': email
-                    },
-                    'action': {
-                        'addLabelIds': ['TRASH'],
-                        'removeLabelIds': ['INBOX']
-                    }
-                }
-                addFilter = service.users().settings().filters().create(userId='me', body=filter_content).execute()
-                emailadded = True
-                print(f'{email} added to {accountname} blacklist.')
-
-        if not emailadded:
-            print(f'No new email added to {accountname}.')
-
-    except HttpError as error:
-        print(f'An error occurred: {error}')
-def scottwork():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    creds = None
-    accountname = 'Scott Work Account'
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    path = '/Users/scott/Learning/Coding/Python3/email-filter/credentials/scott-work-account'
-    #modify the path to the correct credential folder
-
-    if os.path.exists(f'{path}/token.json'):
-        creds = Credentials.from_authorized_user_file(f'{path}/token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                f'{path}/credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open(f'{path}/token.json', 'w') as token:
-            token.write(creds.to_json())
-    df = pd.read_excel(blacklist, engine='openpyxl_wo_formatting')
-
-    try:
-        # Call the Gmail API
-        service = build('gmail', 'v1', credentials=creds)
-        existingfilters = service.users().settings().filters().list(userId='me').execute()
-        # list filters
-        existingfilter = []
-        for item in existingfilters['filter']:
-            if 'from' in item['criteria'].keys():
-                existingfilter.append(item['criteria']['from'])
-        #check existing filters
-
-        emails = df['Email Black List'].tolist()
-        emailadded = False
-        for email in emails:
-            if email not in existingfilter:
-                filter_content = {
-                    'criteria': {
-                        'from': email
-                    },
-                    'action': {
-                        'addLabelIds': ['TRASH'],
-                        'removeLabelIds': ['INBOX']
-                    }
-                }
-                addFilter = service.users().settings().filters().create(userId='me', body=filter_content).execute()
-                emailadded = True
-                print(f'{email} added to {accountname} blacklist.')
-
-        if not emailadded:
-            print(f'No new email added to {accountname}.')
-
-    except HttpError as error:
-        print(f'An error occurred: {error}')
-
-def cafeaccount():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    creds = None
-    accountname = 'Cafe Account'
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    path = '/Users/scott/Learning/Coding/Python3/email-filter/credentials/cafe-account'
-    #modify the path to the correct credential folder
-
-    if os.path.exists(f'{path}/token.json'):
-        creds = Credentials.from_authorized_user_file(f'{path}/token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                f'{path}/credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open(f'{path}/token.json', 'w') as token:
-            token.write(creds.to_json())
-    df = pd.read_excel(blacklist, engine='openpyxl_wo_formatting')
-
-    try:
-        # Call the Gmail API
-        service = build('gmail', 'v1', credentials=creds)
-        existingfilters = service.users().settings().filters().list(userId='me').execute()
-        # list filters
-        existingfilter = []
-        for item in existingfilters['filter']:
-            if 'from' in item['criteria'].keys():
-                existingfilter.append(item['criteria']['from'])
-        #check existing filters
-
-        emails = df['Email Black List'].tolist()
-        emailadded = False
-        for email in emails:
-            if email not in existingfilter:
-                filter_content = {
-                    'criteria': {
-                        'from': email
-                    },
-                    'action': {
-                        'addLabelIds': ['TRASH'],
-                        'removeLabelIds': ['INBOX']
-                    }
-                }
-                addFilter = service.users().settings().filters().create(userId='me', body=filter_content).execute()
-                emailadded = True
-                print(f'{email} added to {accountname} blacklist.')
-
-        if not emailadded:
-            print(f'No new email added to {accountname}.')
-
-    except HttpError as error:
-        print(f'An error occurred: {error}')
-
-def autojob():
-    scottpersonal()
-    roaster()
-    scottwork()
-    cafeaccount()
 
 if __name__ == '__main__':
-#auto run the script
-#     schedule.every(1).hour.do(autojob)
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
-#if want to manually run, use this instead
-    autojob()
+    #------------------------------auto run the script---------------------------------
+    print('Script running at background...')
+    schedule.every(1).hour.do(emailFilter)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+    #-----------------if want to manually run, use this instead------------
+    # emailFilter()
